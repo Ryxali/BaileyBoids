b83bae48fc01657050945be684a84fe29ac1d901
Niklas Lindblad <ryxali@gmail.com>
2016-06-29 08:57:01 +0200

    moves and triggers collisions

---
+++ Assets/Agent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+    private static float WALK_BOUNDS = 4f;
+    private static float WALK_SPEED = 1f;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(BehaviourCycle());
+        AgentProximityDetector.inst.Add(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private IEnumerator BehaviourCycle()
+    {
+        while(true)
+        {
+            yield return StartCoroutine(WalkToPoint(new Vector3
+            {
+                x = Random.Range(-WALK_BOUNDS, WALK_BOUNDS),
+                y = Random.Range(-WALK_BOUNDS, WALK_BOUNDS)
+            }));
+            yield return new WaitForSeconds(Random.Range(0.3f, 1f));
+        }
+        
+    }
+
+
+    private IEnumerator WalkToPoint(Vector3 point)
+    {
+        while(Vector3.Distance(transform.position, point) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, point, Time.deltaTime * WALK_SPEED);
+            yield return null;
+        }
+    }
+   
+
+    public void InteractWith(Agent other)
+    {
+        Debug.Log(name + " talks to " + other.name);
+    }
+}
---
+++ Assets/AgentProximityDetector.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AgentProximityDetector : MonoBehaviour {
+
+    private static AgentProximityDetector _inst;
+    public static AgentProximityDetector inst { get { if (_inst == null) Debug.LogError("No instance of singleton exists"); return _inst; } }
+    private List<Agent> agents;
+    private List<AgentDetection> collisions = new List<AgentDetection>();
+
+    private class AgentDetection
+    {
+        public Agent a { get; set; }
+        public Agent b { get; set; }
+
+        public bool Contains(Agent agent)
+        {
+            return a == agent || b == agent;
+        }
+    }
+
+    void Start()
+    {
+        if (_inst != null) Debug.LogWarning("Multiple instances of singleton exists!");
+        _inst = this;
+        agents = new List<Agent>();
+    }
+
+
+    void Update()
+    {
+        var coll = new List<AgentDetection>();
+        foreach(var agent in agents)
+        {
+            var colliding = agents.FindAll(x => x != agent && Vector3.Distance(agent.transform.position, x.transform.position) < 5f);
+            var newCollisions = colliding.FindAll(x => !coll.Exists(z => z.Contains(x) && z.Contains(agent)));
+            foreach (var val in newCollisions)
+            {
+                coll.Add(new AgentDetection
+                {
+                    a = agent,
+                    b = val
+                });
+            }
+        }
+        var enters = coll.FindAll(x => !collisions.Exists(z => z.Contains(x.a) && z.Contains(x.b)));
+        foreach(var v in enters)
+        {
+            v.a.InteractWith(v.b);
+        }
+        collisions = coll;
+    }
+
+
+    public void Add(Agent agent)
+    {
+        agents.Add( agent);
+    }
+
+}
